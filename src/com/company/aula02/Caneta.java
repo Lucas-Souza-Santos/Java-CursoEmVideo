8d0e159c8772545a3d428dc1e2013d47848f047b
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-19 21:44:52 -0300

    subindo arquivos

--- src/com/company/aula02/Caneta.java
+++ src/com/company/aula02/Caneta.java
@@ -1,4 +1,25 @@
-package com.company;
+package com.company.aula02;
 
-public class Caneta {
+public class Caneta
+{
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status(){
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("está tampada ? " + this.tampada);
+    }
+    void rabiscar(){
+        
+    }
+    void tampar(){
+        this.tampada = true;
+    }
+    void destampar(){
+        this.tampada = false;
+    }
+
 }
